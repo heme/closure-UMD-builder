57c896eb9353b1bf4952a3f048495e397928a233
Eric <git@heme.net>
2013-09-10 15:53:38 -0500

    File that loads Closure "classes" that need to be included

---
+++ src/goog.require.js
@@ -0,0 +1,12 @@
+// Change this to whatever closure-library classes your module needs
+goog.require('goog.array');
+goog.require('goog.dom');
+goog.require('goog.dom.classes');
+goog.require('goog.userAgent');
+goog.require('goog.userAgent.adobeReader');
+goog.require('goog.userAgent.flash');
+goog.require('goog.userAgent.iphoto');
+goog.require('goog.userAgent.jscript');
+goog.require('goog.userAgent.picasa');
+goog.require('goog.userAgent.product');
+goog.require('goog.userAgent.product.isVersion');
